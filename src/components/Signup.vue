0da6bd4af1d2e25a2887e957d94b6786a954b02a
gojohnun <gojohnun@gmail.com>
2025-04-14 18:11:19 +0800

    update tailwindcss version, add daisyui, fix button style

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -6,11 +6,15 @@
 	<div class="top"></div>
 	<div class="text-center">
 		<h1 class="text-xl mt-[43px] mb-[18px]">Welcome Onboard!</h1>
-		<p class="text-sm/6">Let’s help you meet up your task</p>
+		<p class="text-sm/6 text-primary">Let’s help you meet up your task</p>
+		<input type="text">
 	</div>
 	<div class="bottom text-center">
-		<button class="green mb-[46px]">Sign Up</button>
-		<p class="text-[15px] mb-[36px]">Already have an account ? <button>Sign In</button></p>
+		<button class="btn btn-md btn-wide bg-primary text-base-100 font-normal rounded-none h-[44px] mb-[46px]">Sign Up</button>
+		<div class="flex justify-center items-center text-[15px] mb-[36px]">
+			<p class="opacity-62">Already have an account ?</p>
+			<button class="btn btn-link opacity-100">Sign In</button>
+		</div>
 	</div>
 </template>
 <style scoped>
