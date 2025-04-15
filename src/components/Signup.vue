0bc90a4f2f99aecafe5dd6805bfb0770ba8effe4
gojohnun <gojohnun@gmail.com>
2025-04-15 16:51:12 +0800

    add signup input, fix layout

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -3,15 +3,22 @@
 </script>
 
 <template>
-	<div class="top"></div>
-	<div class="text-center">
-		<h1 class="text-xl mt-[43px] mb-[18px]">Welcome Onboard!</h1>
+	<div class="top mt-10 ml">
+		<img src="@/assets/images/back-arrow.svg" />
+	</div>
+	<div class="flex flex-col justify-center content-center text-center">
+		<h1 class="text-xl mb-6">Welcome Onboard!</h1>
 		<p class="text-sm/6 text-primary">Let’s help you meet up your task</p>
-		<input type="text">
+		<fieldset class="fieldset flex flex-col grow gap-6 mt-15 mb-5">
+			<input type="text" class="input w-full h-[47px] border-0 rounded-[11px]" placeholder="Enter your Full Name">
+			<input type="email" class="input w-full h-[47px] border-0 rounded-[11px]" placeholder="Enter your Email address">
+			<input type="password" class="input w-full h-[47px] border-0 rounded-[11px]" placeholder="Create a Password">
+			<input type="password" class="input w-full h-[47px] border-0 rounded-[11px]" placeholder="Confirm your Password">
+		</fieldset>
 	</div>
 	<div class="bottom text-center">
 		<button class="btn btn-md btn-wide bg-primary text-base-100 font-normal rounded-none h-[44px] mb-[46px]">Sign Up</button>
-		<div class="flex justify-center items-center text-[15px] mb-[36px]">
+		<div class="flex justify-center items-center text-[15px]">
 			<p class="opacity-62">Already have an account ?</p>
 			<button class="btn btn-link opacity-100">Sign In</button>
 		</div>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -3,7 +3,7 @@ import Signup from '../components/Signup.vue'
 </script>
 
 <template>
-  <main class="h-[100vh] flex flex-col justify-between content-center">
+  <main class="h-[100vh] flex flex-col justify-between content-center p-6">
     <Signup />
   </main>
 </template>
