d2aa697518eba98d99ebe3e4c58bff1ee12949b9
gojohnun <gojohnun@gmail.com>
2025-06-11 15:42:49 +0800

    fix toastmessage timeout

--- src/components/common/ToastMessage.vue
+++ src/components/common/ToastMessage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { computed, watch } from 'vue'
 import SuccessIcon from '@/components/icons/SuccessIcon.vue'
 import InfoIcon from '@/components/icons/InfoIcon.vue'
 import WarningIcon from '@/components/icons/WarningIcon.vue'
@@ -12,12 +12,18 @@ const showAnimation = computed(() =>
 	? 'animate-jump animate-duration-500 animate-ease-in-out'
 	: 'animate-jump-out animate-duration-300 animate-ease-out'
 )
-onMounted(() => {
-	setTimeout(() => {
-		storeToast.toastData.status = false;
-	}, storeToast.toastData.ms);
+let timeoutId = null;
+watch(() => storeToast.toastData.status, (newStatus) => {
+	if(newStatus) {
+		if(timeoutId) {
+			clearTimeout(timeoutId);
+		}
+		timeoutId = setTimeout(() => {
+      storeToast.toastData.status = false;
+      timeoutId = null; // 計時器執行完畢後將 ID 設為 null
+    }, storeToast.toastData.ms);
+	}
 })
-
 </script>
 <template>
 	<div class="toast toast-center toast-middle animate-once" :class="showAnimation">
