efc7c5a53851641f6fe0597825ba59f8a61e5e60
gojohnun <gojohnun@gmail.com>
2025-06-27 13:45:52 +0800

    [fix] ResetSuccess icon

---
+++ src/components/icons/CheckIcon.vue
@@ -0,0 +1,8 @@
+<template>
+	<svg version="1.1" id="_x31_0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="width: 48px; height: 48px; opacity: 1;" xml:space="preserve">
+	<g>
+		<polygon class="st0" points="431.734,67.163 214.584,284.314 80.266,149.442 0.402,229.306 0.056,229.651 0.056,229.651 0,229.707 
+			214.584,444.837 512,147.429 	" style="fill: rgb(85, 132, 122);"></polygon>
+	</g>
+	</svg>
+</template>
--- src/views/ResetSuccessView.vue
+++ src/views/ResetSuccessView.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
   import { useRouter } from 'vue-router';
   const router = useRouter();
-  import SuccessIcon from '@/components/icons/SuccessIcon.vue'
+  import CheckIcon from '@/components/icons/CheckIcon.vue'
   import { useI18n } from 'vue-i18n';
   const { t } = useI18n();
 </script>
 
 <template>
   <main class="flex-1 flex items-center">
-  <div class="text-center">
-    <div class="flex items-center justify-center text-base font-semibold" :style="{color: 'var(--color-green)'}"><SuccessIcon /></div>
+  <div class="w-full text-center">
+    <div class="flex items-center justify-center text-base font-semibold" :style="{color: 'var(--color-green)'}"><CheckIcon /></div>
     <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">{{ t('resetsuccess.title') }}</h1>
     <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">{{ t('resetsuccess.subtitle') }}</p>
     <div class="mt-10 flex items-center justify-center">
